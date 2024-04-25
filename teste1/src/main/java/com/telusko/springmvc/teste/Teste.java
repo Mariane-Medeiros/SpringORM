fdde074ecd850205f35e2d8c668a0091e9a15fba
Mariane-Medeiros <mariane.oli.med@gmail.com>
2024-04-25 16:07:07 -0300

    progresso1

--- teste1/src/main/java/com/telusko/springmvc/teste/Teste.java
+++ teste1/src/main/java/com/telusko/springmvc/teste/Teste.java
@@ -3,11 +3,21 @@ package com.telusko.springmvc.teste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Teste {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Teste.class, args);
+	}
+	public class ServletInitializer extends SpringBootServletInitializer {
+
+	    @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(SpringBootApplication.class);
+	    }
+
 	}
 }
